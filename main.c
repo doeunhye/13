20691073e7715c5131a23e53405756b429f064da
doeunhye <ehdmsgp123@naver.com>
2018-11-29 16:07:15 +0900

    2

--- main.c
+++ main.c
@@ -3,11 +3,62 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int is_whiltespace(char c)
+{
+	if(c == ' '||
+	   c == '\t'||
+	   c == '\n'||
+	   c == '\r'||
+	   c == '('||
+	   c == ')')
+	   return 1;
+	   
+	return 0;
+}
+
+int fget_word(FILE *fp, char *word)
+{
+	char c;
+	int cnt;
+		
+	while ((c=fget(fp)) != EOF)
+	{
+	if (is_whitespace(c) == 0 )
+		{
+			break;
+		}
+			
+	if ( c == EOF)
+	{
+		return 0;
+	}
+	
+	cnt = 0;
+	word[cnt++] = c;
+	word[cnt] = '\0';
+	
+	while((word[cnt]=fgetc(fp)) != EOF){
+		if (is_whitespace(word[cnt])==1)
+		{
+			word[cnt] = '\0';
+			break;
+		}
+		cnt++;
+	}
+	
+	return cnt;
+}
+	
+
+
+
 int main(int argc, char *argv[]) {
 	
 	FILE* fp;
 	char name[100];
+	char word[100];
 	
+
 	//printf, scanf
 	printf("input a file name : ");
 	scanf("%s",name);
@@ -20,10 +71,18 @@ int main(int argc, char *argv[]) {
 		printf("error! check file name : %s \n", name);
 		return -1;
 	}
+	while (fget_word ( fp, word)!=0)
+	{
+		printf("%s\n", word);
+	}
 	
-	printf("%c\n", fgetc(fp));
+	fclose(fp);
+	}
 	
-	//fclose 
+	//word reading
+	
+	
+	
 	
 	return 0;
 }
